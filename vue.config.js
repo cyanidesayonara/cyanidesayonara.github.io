d8a02708a034236623d6fd33943ca5793f453f5a
santtu <cyanidesayonara@gmail.com>
2021-07-11 02:53:56 +0300

    add blog & upgrade to vue 3

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   publicPath: "/",
+  chainWebpack(config) {
+    config.module
+      .rule("md")
+      .test(/\.md/)
+      .use("vue-loader")
+      .loader("vue-loader")
+      .end()
+      .use("vue-markdown-loader")
+      .loader("vue-markdown-loader/lib/markdown-compiler")
+      .options({
+        raw: true,
+      });
+  },
 };
