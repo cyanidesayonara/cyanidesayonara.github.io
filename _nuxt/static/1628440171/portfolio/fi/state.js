0a619f46ab70db9d9d827f4f0adbb75fb44fac41
cyanidesayonara <cyanidesayonara@users.noreply.github.com>
2021-08-08 16:29:33 +0000

    deploy: 8d8533d917e7365cf8c46a4d1174e54581657522

---
+++ _nuxt/static/1628440171/portfolio/fi/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcyanidesayonara.github.io\u002F_nuxt\u002Fstatic\u002F1628440171",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio\u002Ffi",config:{_app:{basePath:"\u002Fcyanidesayonara.github.io\u002F",assetsPath:"\u002Fcyanidesayonara.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6371f0a5"}}}}(null));
