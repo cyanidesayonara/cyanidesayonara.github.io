ad7cb13d3c613069bb5d44fc26c61cbde08254af
cyanidesayonara <cyanidesayonara@users.noreply.github.com>
2021-08-15 15:35:38 +0000

    deploy: f465dc2115c4ce49cbb64a932d3d37473b78239b

--- _nuxt/static/1628447918/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628447918",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"368c2892"}}}}(null,"\u002F"));
