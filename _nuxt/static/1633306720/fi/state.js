5bf79873aeb6fba5014482166ac4d6bfd114bf8c
cyanidesayonara <cyanidesayonara@users.noreply.github.com>
2021-10-04 19:14:03 +0000

    deploy: febeeb398fc02aaf647124149464c63bd46d296b

--- _nuxt/static/1633306720/fi/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633306720",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c0eae07e"}}}}(null));
